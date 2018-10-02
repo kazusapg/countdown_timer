f30734f4efcb47f2d06ad1e43a28da4f84ccb651
kazusa_pg <kazusaprog@gmail.com>
2018-10-02 20:36:21 +0900

    Classに変更

--- countdown_timer.py
+++ countdown_timer.py
@@ -1,30 +1,39 @@
+# TODO:Pause機能
+# TODO:残り時間の表示
+
 import argparse
 import pygame
 import time
 import sys
 
+class CountDownTimer:
 
-def count_down(minutes, mp3file_path):
-    seconds = minutes * 60
-    while seconds >= 0:
-        seconds -= 1
-        time.sleep(1)
-    pygame.mixer.init()
-    pygame.mixer.music.load(mp3file_path)
-    print('時間になりました')
-    try:
-        while True:
-            pygame.mixer.music.play(100)
-            if pygame.mixer.music.get_busy():
-                time.sleep(1)
-    except KeyboardInterrupt:
-        sys.exit()
+    def __init__(self, minutes, mp3_filepath):
+        self.minutes = minutes
+        self.mp3_file_path = mp3_filepath
+
+    def count_down(self):
+        seconds = self.minutes * 60
+        while seconds >= 0:
+            seconds -= 1
+            time.sleep(1)
+        pygame.mixer.init()
+        pygame.mixer.music.load(self.mp3_file_path)
+        print('時間になりました')
+        try:
+            while True:
+                pygame.mixer.music.play(100)
+                if pygame.mixer.music.get_busy():
+                    time.sleep(1)
+        except KeyboardInterrupt:
+            sys.exit()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='引数で指定した時間（分単位）後にmp3ファイルを鳴らす')
     parser.add_argument('minutes', type=int, help='指定時間（分単位）')
-    parser.add_argument('mp3file_path', type=str, help='mp3ファイルのパス')
+    parser.add_argument('mp3_file_path', type=str, help='mp3ファイルのパス')
 
     args = parser.parse_args()
-    count_down(args.minutes, args.mp3file_path)
+    countdown_timer = CountDownTimer(args.minutes, args.mp3_file_path)
+    countdown_timer.count_down()
